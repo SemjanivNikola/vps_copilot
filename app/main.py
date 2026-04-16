a925f448188dff9eadea97302c3a235f73b16aed
nikola.semjaniv <nikola.semjaniv@pis.eu.com>
2026-04-16 13:02:18 +0200

    Add slowapi package (request rate limiter)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from dotenv import load_dotenv
-
 from fastapi import FastAPI
 from app.api.router import api_router
+from dotenv import load_dotenv
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 load_dotenv()
+limiter = Limiter(key_func=get_remote_address)
 
 """
 Disable the automatic docs endpoints in production until needed. If needed internaly protect them behind auth
@@ -18,5 +20,6 @@ app.include_router(api_router, prefix="/api")
 
 
 @app.get("/")
+@limiter.limit("5/minute")
 def root():
     return {"status": "ok"}
